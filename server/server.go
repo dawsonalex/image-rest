582858e81b4f64fa767c5309ff806497d6dc0b09
Alexander Dawson <alexander_dawson@ymail.com>
2021-04-17 17:24:01 +0100

    Testing alternative upload function

--- server/server.go
+++ server/server.go
@@ -3,16 +3,13 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dawsonalex/image-rest/imageservice"
+	"github.com/sirupsen/logrus"
 	"io"
-	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sort"
-	"strings"
-
-	"github.com/dawsonalex/image-rest/imageservice"
-	"github.com/sirupsen/logrus"
 )
 
 // FilesHandler returns a http.HandlerFunc that accepts requests for an image stores
@@ -105,110 +102,151 @@ func ImageHandler(dir string, logger *logrus.Logger) http.HandlerFunc {
 	})
 }
 
-// UploadHandler handles requests to upload files to the server.
+//// UploadHandler handles requests to upload files to the server.
+//func UploadHandler(uploadDir string, logger *logrus.Logger) http.HandlerFunc {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		if !(r.Method == http.MethodPost) {
+//			logger.Errorf("Invalid HTTP method, got: %v", r.Method)
+//			w.WriteHeader(http.StatusMethodNotAllowed)
+//			return
+//		}
+//
+//		// define some variables used throughout the function
+//		// n: for keeping track of bytes read and written
+//		// err: for storing errors that need checking
+//		var n int
+//		var err error
+//
+//		// define pointers for the multipart reader and its parts
+//		var mr *multipart.Reader
+//		var part *multipart.Part
+//
+//		if mr, err = r.MultipartReader(); err != nil {
+//			logger.Errorf("Error opening multipart reader: %v", err)
+//			w.WriteHeader(http.StatusInternalServerError)
+//			fmt.Fprintf(w, "Error occured during upload")
+//			return
+//		}
+//
+//		// buffer to be used for reading bytes from files
+//		chunk := make([]byte, 4096)
+//		fileBytes := make([]byte, 0)
+//
+//		contentTypeChecked := false
+//		for {
+//			// flag for tracking when the end of a part is reached.
+//			var uploaded bool
+//
+//			if part, err = mr.NextPart(); err != nil {
+//				if err != io.EOF {
+//					logger.Errorf("Error occurred while fetching next part: %v", err)
+//
+//					w.WriteHeader(http.StatusInternalServerError)
+//					fmt.Fprintf(w, "Error occured during upload")
+//				} else {
+//					w.WriteHeader(http.StatusOK)
+//					fmt.Fprintf(w, "")
+//				}
+//				return
+//			}
+//
+//			// Read in the next chunk
+//			for !uploaded {
+//				// If we get an error reading the chunk EOF indicates chunk is done
+//				// any other error is a problem.
+//				if n, err = part.Read(chunk); err != nil {
+//					if err != io.EOF {
+//						logger.Errorf("Error occurred reading chunk: %v", err)
+//
+//						w.WriteHeader(http.StatusInternalServerError)
+//						fmt.Fprintf(w, "Error occured during upload")
+//						return
+//					}
+//					uploaded = true
+//				}
+//
+//				// If we haven't tested the content type of the actual file,
+//				// do it now. Stop the upload if the file isn't an image.
+//				if !contentTypeChecked {
+//					contentType := http.DetectContentType(chunk)
+//					logger.Debugf("UploadHandler(): got image of content type %s", contentType)
+//					isImage := strings.Contains(contentType, "image/")
+//					if !isImage {
+//						logger.Errorf("HandleUpload(): attempted to upload non-image file: %s", part.FileName())
+//						http.Error(w, "Request content is not an image", http.StatusUnsupportedMediaType)
+//						return
+//					}
+//					contentTypeChecked = true
+//				}
+//
+//				// append this chunk to the whole file bytes in memory.
+//				fileBytes = append(fileBytes, chunk[:n]...)
+//			}
+//
+//			// initially, write the file to temp dir,
+//			// then we'll copy the file to the watch dir in one go.
+//			// This avoids the imageservice getting multiple write
+//			// events when the file isn't fully loaded from the network.
+//			imgPath := filepath.Join(uploadDir, part.FileName())
+//			imageFile, err := os.Create(imgPath)
+//			if err != nil {
+//				logger.Errorf("Error occurred while creating image file: %v", err)
+//
+//				w.WriteHeader(http.StatusInternalServerError)
+//				fmt.Fprintf(w, "Error occured during upload")
+//				return
+//			}
+//			defer imageFile.Close()
+//
+//			// write the whole file to disc
+//			if _, err = imageFile.Write(fileBytes[:]); err != nil {
+//				logger.Errorf("Error occurred writing chunk to save file: %v", err)
+//				w.WriteHeader(http.StatusInternalServerError)
+//				fmt.Fprintf(w, "Error occured during upload")
+//				return
+//			}
+//			logger.Debugf("http: saved file %s", imageFile.Name())
+//		}
+//	})
+//}
+
 func UploadHandler(uploadDir string, logger *logrus.Logger) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !(r.Method == http.MethodPost) {
-			logger.Errorf("Invalid HTTP method, got: %v", r.Method)
-			w.WriteHeader(http.StatusMethodNotAllowed)
+		//get the multipart reader for the request.
+		reader, err := r.MultipartReader()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// define some variables used throughout the function
-		// n: for keeping track of bytes read and written
-		// err: for storing errors that need checking
-		var n int
-		var err error
-
-		// define pointers for the multipart reader and its parts
-		var mr *multipart.Reader
-		var part *multipart.Part
-
-		if mr, err = r.MultipartReader(); err != nil {
-			logger.Errorf("Error opening multipart reader: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "Error occured during upload")
-			return
-		}
-
-		// buffer to be used for reading bytes from files
-		chunk := make([]byte, 4096)
-		fileBytes := make([]byte, 0)
-
-		contentTypeChecked := false
+		//copy each part to destination.
 		for {
-			// flag for tracking when the end of a part is reached.
-			var uploaded bool
-
-			if part, err = mr.NextPart(); err != nil {
-				if err != io.EOF {
-					logger.Errorf("Error occurred while fetching next part: %v", err)
-
-					w.WriteHeader(http.StatusInternalServerError)
-					fmt.Fprintf(w, "Error occured during upload")
-				} else {
-					w.WriteHeader(http.StatusOK)
-					fmt.Fprintf(w, "")
-				}
-				return
+			part, err := reader.NextPart()
+			if err == io.EOF {
+				break
 			}
 
-			// Read in the next chunk
-			for !uploaded {
-				// If we get an error reading the chunk EOF indicates chunk is done
-				// any other error is a problem.
-				if n, err = part.Read(chunk); err != nil {
-					if err != io.EOF {
-						logger.Errorf("Error occurred reading chunk: %v", err)
-
-						w.WriteHeader(http.StatusInternalServerError)
-						fmt.Fprintf(w, "Error occured during upload")
-						return
-					}
-					uploaded = true
-				}
-
-				// If we haven't tested the content type of the actual file,
-				// do it now. Stop the upload if the file isn't an image.
-				if !contentTypeChecked {
-					contentType := http.DetectContentType(chunk)
-					logger.Debugf("UploadHandler(): got image of content type %s", contentType)
-					isImage := strings.Contains(contentType, "image/")
-					if !isImage {
-						logger.Errorf("HandleUpload(): attempted to upload non-image file: %s", part.FileName())
-						http.Error(w, "Request content is not an image", http.StatusUnsupportedMediaType)
-						return
-					}
-					contentTypeChecked = true
-				}
-
-				// append this chunk to the whole file bytes in memory.
-				fileBytes = append(fileBytes, chunk[:n]...)
+			//if part.FileName() is empty, skip this iteration.
+			if part.FileName() == "" {
+				continue
 			}
-
-			// initially, write the file to temp dir,
-			// then we'll copy the file to the watch dir in one go.
-			// This avoids the imageservice getting multiple write
-			// events when the file isn't fully loaded from the network.
 			imgPath := filepath.Join(uploadDir, part.FileName())
-			imageFile, err := os.Create(imgPath)
+			dst, err := os.Create(imgPath)
+			defer dst.Close()
+
 			if err != nil {
-				logger.Errorf("Error occurred while creating image file: %v", err)
-
-				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintf(w, "Error occured during upload")
+				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			defer imageFile.Close()
 
-			// write the whole file to disc
-			if _, err = imageFile.Write(fileBytes[:]); err != nil {
-				logger.Errorf("Error occurred writing chunk to save file: %v", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintf(w, "Error occured during upload")
+			if _, err := io.Copy(dst, part); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			logger.Debugf("saved file %s", imageFile.Name())
 		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "")
 	})
 }
